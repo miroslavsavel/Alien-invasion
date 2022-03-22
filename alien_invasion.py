c102d9317e8813215e47c83c85a2722b005de558
Miroslav Savel <miroslav.savel@gmail.com>
2022-03-22 15:46:45 +0100

    End game with "q"

--- alien_invasion.py
+++ alien_invasion.py
@@ -44,6 +44,8 @@ class AlienInvasion:
         elif event.key ==pygame.K_LEFT:
             # move ship to the left
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
