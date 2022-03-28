f7a7fea7f61111816853977c5e276d8ba32b4a56
Miroslav Savel <miroslav.savel@gmail.com>
2022-03-28 12:37:02 +0200

    Bullet class that inherit from Sprites

--- settings.py
+++ settings.py
@@ -7,5 +7,11 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
-
         self.ship_speed = 1.5
+
+        # Bullet settings
+        # 3x5 pixel gray bullet, slower than ship
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
